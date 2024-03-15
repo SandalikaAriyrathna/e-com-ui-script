b1d4430a8dfd9f6d4764c12b1a2c13df89b74951
SandalikaAriyrathna <dilsandu96@gmail.com>
2024-03-15 19:45:36 +0530

    new modal

--- modal-script.js
+++ modal-script.js
@@ -1,38 +1,41 @@
+// Get the modal
+var modal = document.getElementById("modal");
 
+// Get the <span> element that closes the modal
+var closeBtn = document.getElementsByClassName("close")[0];
 
-    // Get the modal
-    const modal = document.getElementById("modal");
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
 
-    // Get the button that opens the modal
-    const openModalButton = document.getElementById("openModalButton");
+// Function to show the modal if an hour has passed
+function showModalIfNeeded() {
+  const lastShown = localStorage.getItem('lastShown');
+  const now = new Date().getTime();
 
-    // Get the <span> element that closes the modal
-    const closeBtn = document.getElementsByClassName("close")[0];
+  // Show if more than an hour has passed or never shown
+  if (!lastShown || now - lastShown > 60000) { // 3600000 ms = 1 hour
+    modal.style.display = "block";
+    localStorage.setItem('lastShown', now.toString());
+  }
+}
 
-    // Function to show the modal if an hour has passed
-    function showModalIfNeeded() {
-        const lastShown = localStorage.getItem('lastShown');
-        const now = new Date().getTime();
+// Call showModalIfNeeded on page load
+window.onload = showModalIfNeeded;
 
-        // Show if more than an hour has passed or never shown
-        if (!lastShown || now - lastShown > 60000) {
-            modal.style.display = "block";
-            localStorage.setItem('lastShown', now.toString());
-        }
-    }
+// Add event listener for the 'openModalButton'
+document.getElementById("openModalButton").onclick = function() {
+  modal.style.display = "block";
+};
 
-    // Call showModalIfNeeded on page load
-    window.onload = showModalIfNeeded;
-
-    // When the user clicks on <span> (x), close the modal
-    closeBtn.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks on the save preferences button, close the modal
-    document.getElementById("savePreferences").onclick = function() {
-        modal.style.display = "none";
-        const selectedStyle = document.getElementById("stylePreference").value;
-        alert("You chose the " + selectedStyle + " style.");
-        // Additional logic to save preferences goes here
-    }
+// Get the brand elements and attach click event listeners
+document.querySelectorAll('.brand').forEach(function(brand) {
+  brand.addEventListener('click', function() {
+    // Here you can add code to handle the click event
+    // For example, saving the user's preference or displaying more information
+    modal.style.display = "none";
+    console.log("Brand selected: " + this.alt);
+    // Implement your logic to display user experience or store preference
+  });
+});
